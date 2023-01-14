453d07aa5eb79d35988990545ada51a317e330bf
Jony Melo <jony.everton-@hotmail.com>
2023-01-14 03:39:46 -0300

    Projeto Finalizado

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key ="87f04a60c0b3879ec6c710c0769c8352"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
